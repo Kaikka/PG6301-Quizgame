1a98fdb9f92297dc07e86b203efcf811517e8179
Kaikka <kaileeto@gmail.com>
2021-04-16 20:21:35 +0200

    exported code to match.jsx

--- quizgame/src/index.jsx
+++ quizgame/src/index.jsx
@@ -1,44 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {getRandomQuizzes} from "./quizzes";
+import {Match} from "./match";
 
+const App = () => {
 
-class App extends React.Component {
+    return(
+        <Match/>
+    )
 
-    constructor(props) {
-        super(props);
-
-        this.state = {quiz: getRandomQuizzes(1)[0]}
-    }
-
-    handleClick = (correct) => {
-        if (correct) {
-            alert('Correct!!!');
-            this.setState({quiz: getRandomQuizzes(1)[0]})
-        } else {
-            alert('Wrong answer');
-        }
-    };
-
-    renderAnswerTag(answer, correct) {
-
-        //let onclick = correct ? "alert(\"Correct!\"); this.setState({quiz: getRandomQuizzes(1)[0]});" : "alert(\"Wrong!\");";
-        //return <div className="answer" onClick={() => onclick}>{answer}</div>;
-
-        return <div className="answer" key={answer} onClick={() => this.handleClick(correct)}> {answer} </div>;
-    }
-
-    render() {
-
-        const quiz = this.state.quiz;
-
-        return (
-            <>
-                <p className='question'>Question: {quiz.question} </p>
-                {quiz.answers.map(e => this.renderAnswerTag(e, quiz.answers.indexOf(e) === quiz.correctAnswerIndex))}
-            </>
-        )
-    }
 }
 
 ReactDOM.render(<App/>, document.getElementById("root"));
