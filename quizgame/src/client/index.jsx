3735cf4934c64c05e9dea5282be4d60fd86bb156
Kaikka <kaileeto@gmail.com>
2021-04-17 22:06:48 +0200

    its all working, yarn start, ex05 done

--- quizgame/src/client/index.jsx
+++ quizgame/src/client/index.jsx
@@ -3,11 +3,30 @@ import ReactDOM from "react-dom";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import {Match} from "./match";
+import {Home} from "./home";
+
+const NotFound = () => {
+
+    return (
+        <div>
+            <h2>NOT FOUND: 404</h2>
+            <p>
+                ERROR: the paw you requwuested is not availible :3
+            </p>
+        </div>
+    )
+}
 
 const App = () => {
 
     return(
-        <Match/>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path={"/"} component={Home} />
+                <Route exact path={"/match"} component={Match} />
+                <Route component={NotFound}/>
+            </Switch>
+        </BrowserRouter>
     )
 
 }
--- quizgame/src/client/match.jsx
+++ quizgame/src/client/match.jsx
@@ -6,32 +6,99 @@ export class Match extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {quiz: getRandomQuizzes(1)[0]}
+        //this.state = {quiz: getRandomQuizzes(1)[0]}
+        this.state = {
+            match: null
+        }
     }
+
+    componentDidMount() {
+        this.startNewMatch();
+    }
+
+    startNewMatch = () => {
+
+        const quizzes = getRandomQuizzes(3);
+
+        this.setState({
+            match: {
+                victory: false,
+                defeat: false,
+                quizzes: quizzes,
+                currentIndex: 0,
+                numberOfQuizzes: quizzes.length
+            }
+        });
+
+    };
 
     handleClick = (correct) => {
         if (correct) {
-            alert('Correct!!!');
-            this.setState({quiz: getRandomQuizzes(1)[0]})
+            if (this.state.match.currentIndex === (this.state.match.numberOfQuizzes - 1)) {
+                //last quiz
+                this.setState({match: {victory: true}});
+            } else {
+                //go on to next quiz
+                this.setState(prev => ({
+                    match: {
+                        currentIndex: prev.match.currentIndex + 1,
+                        quizzes: prev.match.quizzes,
+                        numberOfQuizzes: prev.match.numberOfQuizzes
+                    }
+                }));
+            }
+
         } else {
-            alert('Wrong answer');
+            this.setState({match: {defeat: true}});
         }
     };
 
     renderAnswerTag(answer, correct) {
 
-        return <div className="answer" key={answer} onClick={() => this.handleClick(correct)}> {answer} </div>;
+        return <div className="answer" key={answer} onClick={() => this.handleClick(correct)} tabIndex="0"> {answer} </div>;
     }
 
     render() {
 
-        const quiz = this.state.quiz;
+        if (!this.state.match) {
+            return <h2>Loading...</h2>;
+        }
+
+        if (this.state.match.victory) {
+            return (
+                <div className="game-result">
+                    <h2>You Won!</h2>
+                    <div className="action">
+                        <button className="play new-game-button" onClick={this.startNewMatch}>New Match</button>
+                    </div>
+                </div>
+            );
+        }
+
+        if (this.state.match.defeat) {
+            return (
+                <div className="game-result">
+                    <h2>Wrong Answer! You Lost!</h2>
+                    <div className="action">
+                        <button className="play new-game-button" onClick={this.startNewMatch}>New Match</button>
+                    </div>
+                </div>
+            );
+        }
+
+        //const quiz = this.state.quiz;
+
+        const match = this.state.match;
+        const count = "" + (match.currentIndex + 1) + "/" + match.numberOfQuizzes;
+        const quiz = match.quizzes[match.currentIndex];
 
         return (
             <>
-                <p className='question'>Question: {quiz.question} </p>
+                <p className='question'>Question {count}: {quiz.question} </p>
                 {quiz.answers.map(e => this.renderAnswerTag(e, quiz.answers.indexOf(e) === quiz.correctAnswerIndex))}
             </>
         )
     }
+
+
 }
